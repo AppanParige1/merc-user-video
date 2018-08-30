232dbdeb2c40f27dc0f6c13893c5dde05b9c957d
Lokanadham <d.lokanadham@gmail.com>
2018-08-30 11:44:07 +0530

    developed userdetailspage

--- src/api/user/userUpdateApiService.js
+++ src/api/user/userUpdateApiService.js
@@ -3,10 +3,4 @@ import { updateProxy } from '../updateApiProxy';
 
 const url = config.userApiUrl;
 
-export const updateUser = (userId, userDetails) =>
-
-  updateProxy(url+userId, userDetails).then(function (userDetails) {
-      return true;
-  }, function (error) {
-      console.log(error);
-  });
+export const updateUser = (userId, userDetails) => updateProxy(url+userId, userDetails);
--- src/userSection/user/UserDetailsPage.js
+++ src/userSection/user/UserDetailsPage.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import * as userReadApiService from '../../api/user/userReadApiService';
+import * as userUpdateApiService from '../../api/user/userUpdateApiService';
 import { Header } from '../../shared/Header';
 import * as videoApiService from '../../api/video/videoApiService';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 class UserDetailsPage extends Component {
     state = {
         userDetails: null,
-        allVideos: null
+        allVideos: null,
+        selectedVideoIds:[]
     }
+
+    constructor() {
+        super();
+
+        this.state = {
+            userDetails: null,
+            allVideos: null,
+            selectedVideoIds:[]
+        }
+
+        this.updateSelectedVideos = this.updateSelectedVideos.bind(this);
+        this.updateVideos = this.updateVideos.bind(this);
+
+    } 
 
     getVideoList = () => {
         return videoApiService.getAllVideos().then(function (videos) {
@@ -47,6 +71,7 @@ class UserDetailsPage extends Component {
         //this.getUserDetails(userId);
 
 
+         //Mock Implementation- one api is ready we can  remove below 2 lines
         const mockvideoList =  [
             {
                 "VideoId": 23, "VideoName": "FIFA World cup",
@@ -57,6 +82,11 @@ class UserDetailsPage extends Component {
                 "VideoId": 24, "VideoName": "Mickymouse",
                 "VideoGroups": [{ "GroupId": 14, "GroupName": "Comic", "Videos ": null },
                 { "GroupId": 15, "GroupName": "Movies", "Videos": null }]
+            },
+            {
+                "VideoId": 24, "VideoName": "Bigboss",
+                "VideoGroups": [{ "GroupId": 14, "GroupName": "Realtyshow", "Videos ": null },
+                { "GroupId": 15, "GroupName": "Serial", "Videos": null }]
             }
         ]
         this.setState({ allVideos: mockvideoList })
@@ -65,25 +95,71 @@ class UserDetailsPage extends Component {
         //this.getVideoList();
     }
 
+    updateVideos =()=>{
+        userUpdateApiService.updateUser(this.state.userDetails.UserId, this.state.selectedVideoIds).then((isUpdated)=>{
+            if(isUpdated){
+               return this.getUserDetails(this.state.userDetails.UserId)
+            }
+        });
+    }
+    
+    updateSelectedVideos=(event, value)=> {
+        let selectedVideoId = event.target.attributes.value.value;
+        var selectVidoes = this.state.selectedVideoIds;
+        selectVidoes.push(selectedVideoId);
+
+        this.setState({selectedVideoIds:selectVidoes});
+    };
+
     render() {
-        const styles = {
-           
-        };
+               const styles ={
+                   listStyle:{
+                    width: '30%',
+                    float: 'left',
+                    border:'1px solid gray',
+                    height:'500px'
+                   }
+               }
         return (
             <div >
                   <Header title={this.state.userDetails && this.state.userDetails.UserName} />
-                  <div>
-                     {this.state.allVideos && this.state.allVideos.map(element => {
-                        return <span key={element.VideoName}>{element.VideoName}</span>
-                    })}
-                     </div>
-                  <div>
-                   {this.state.userDetails && this.state.userDetails.Videos.map(element => {
-                        return <span key={element.VideoName}>{element.VideoName}</span>
-                    })}
+                    <div style={styles.listStyle}>
+                        <List subheader={<ListSubheader>Available Videos</ListSubheader>}>
+                            {this.state.allVideos && this.state.allVideos.map(element => (
+                                <ListItem
+                                    key={element.VideoName}
+                                    dense
+                                    button
+                                >
+                                    <Checkbox
+                                        tabIndex={-1}
+                                        value ={element.VideoId}
+                                        onChange={this.updateSelectedVideos}
+                                    />
+                                    <ListItemText primary={element.VideoName} />
+                                </ListItem>
+                            ))}
+                        </List>
                     </div>
-                  
-            </div>
+                    <div style={{ float: 'left', paddingTop: '240px' }}>
+                        <Button color="primary" onClick={this.updateVideos} >
+                            Assign
+                        </Button>
+                    </div>
+                    <div style={styles.listStyle}>
+                        <List  subheader={<ListSubheader>Assigned Videos</ListSubheader>}>
+                            {this.state.userDetails && this.state.userDetails.Videos.map(element => (
+                                <ListItem
+                                    key={element.VideoName}
+                                    dense
+                                    button
+                                >
+                                    <ListItemText primary={element.VideoName} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                    </div>
         );
     }
 }
